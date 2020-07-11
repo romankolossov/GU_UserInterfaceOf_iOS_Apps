072fc5b3e1c1e7e48e522376c61ac881f900a90c
romankolossov <rkolossov@gmail.com>
2020-07-11 22:41:14 +0300

    Lesson3 home work in progress 10

--- vkAlissia/vkAlissia/Friends/FriendsViewController.swift
+++ vkAlissia/vkAlissia/Friends/FriendsViewController.swift
@@ -13,8 +13,8 @@ class FriendsViewController: UIViewController {
     
     var friends: Array<FriendData> = [
         FriendData(friendName: "Алиска", friendImage: UIImage(imageLiteralResourceName: "Alissia0")),
-        FriendData(friendName: "Барсик", friendImage: UIImage(imageLiteralResourceName: "Alissia1")),
-        FriendData(friendName: "Васька", friendImage: UIImage(imageLiteralResourceName: "Alissia2")),
+        FriendData(friendName: "Барсика", friendImage: UIImage(imageLiteralResourceName: "Alissia1")),
+        FriendData(friendName: "Василиса", friendImage: UIImage(imageLiteralResourceName: "Alissia2")),
         FriendData(friendName: "Стеша", friendImage: UIImage(imageLiteralResourceName: "Alissia3")),
         FriendData(friendName: "Пушок", friendImage: UIImage(imageLiteralResourceName: "Alissia4")),
         FriendData(friendName: "Фекла", friendImage: UIImage(imageLiteralResourceName: "Alissia5")),
@@ -22,8 +22,8 @@ class FriendsViewController: UIViewController {
         FriendData(friendName: "Мурка", friendImage: UIImage(imageLiteralResourceName: "Alissia7")),
         FriendData(friendName: "Счастливчик", friendImage: UIImage(imageLiteralResourceName: "Alissia8")),
         FriendData(friendName: "Забияка", friendImage: UIImage(imageLiteralResourceName: "Alissia9")),
-        FriendData(friendName: "Борька", friendImage: UIImage(imageLiteralResourceName: "Alissia10")),
-        FriendData(friendName: "Принц", friendImage: UIImage(imageLiteralResourceName: "Alissia11")),
+        FriendData(friendName: "Мурлыка", friendImage: UIImage(imageLiteralResourceName: "Alissia10")),
+        FriendData(friendName: "Принцесса", friendImage: UIImage(imageLiteralResourceName: "Alissia11")),
         FriendData(friendName: "Софочка", friendImage: UIImage(imageLiteralResourceName: "Alissia12")),
     ]
     
