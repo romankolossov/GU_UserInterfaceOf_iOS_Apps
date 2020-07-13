994bca518bea344c52436adcbcd088c645ea1299
romankolossov <rkolossov@gmail.com>
2020-07-14 00:01:05 +0300

    Lesson4 home work shadow task done

---
+++ vkAlissia/vkAlissia/Friends/ShadowView.swift
@@ -0,0 +1,26 @@
+//
+//  ShadowView.swift
+//  vkAlissia
+//
+//  Created by Роман Колосов on 13.07.2020.
+//  Copyright © 2020 Roman N. Kolosov. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.drawShadow()
+    }
+    
+    func drawShadow() {
+        self.layer.cornerRadius = self.frame.size.height / 2
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowRadius = 8
+        self.layer.shadowOffset = CGSize.zero
+    }
+}
